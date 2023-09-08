766dfdfb4e0b0161cd65fd0e1b8f7e973d32939f
Oleksandr Oleksenko <Alex@Orynas-MBP.hitronhub.home>
2023-09-08 12:09:44 -0400

    First iOS project

---
+++ LoginScreenUI/LoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenUIApp.swift
+//  LoginScreenUI
+//
+//  Created by Oleksandr Oleksenko on 2023-09-08.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
